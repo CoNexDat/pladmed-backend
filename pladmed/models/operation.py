18cc6c0e9f64247a15d43c4c593f750044e84eea
fedefunes96 <fede.funes96@gmail.com>
2020-12-15 20:32:56 -0300

    Agrego identificador al modelo de operaciones

--- pladmed/models/operation.py
+++ pladmed/models/operation.py
@@ -1,5 +1,6 @@
 class Operation:
-    def __init__(self, operation, params, probes):
+    def __init__(self, _id, operation, params, probes):
+        self._id = _id
         self.operation = operation
         self.probes = probes
         self.params = params
