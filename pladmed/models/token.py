2b926580ef84cfde095106f519e49f026cbf6e28
fedefunes96 <fede.funes96@gmail.com>
2020-12-14 21:39:59 -0300

    Agrego metodos para crear e identificar identidades desde un token

--- pladmed/models/token.py
+++ pladmed/models/token.py
@@ -3,3 +3,9 @@ import jwt
 class Token:
     def __init__(self, secret_key):
         self.secret_key = secret_key
+
+    def create_token(self, identity):
+        return jwt.encode(identity, self.secret_key, algorithm='HS256')
+    
+    def identity(self, token):
+        return jwt.decode(token, self.secret_key, algorithms=['HS256'])
--- pladmed/tests/unit_tests/test_token.py
+++ pladmed/tests/unit_tests/test_token.py
@@ -7,3 +7,14 @@ class TokenTest(unittest.TestCase):
 
     def test_token_has_secret_key(self):
         self.assertEqual(self.token.secret_key, "secret_key")
+
+    def test_token_encodes_identity(self):
+        identity = {
+            "email": "agustin@gmail.com"
+        }
+        
+        token = self.token.create_token(identity)
+
+        ret_identity = self.token.identity(token)
+
+        self.assertEqual(identity["email"], ret_identity["email"])
