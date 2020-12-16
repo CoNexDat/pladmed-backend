49f1d0359c2b7deea3c1766f008200e65dc90065
fedefunes96 <fede.funes96@gmail.com>
2020-12-15 22:32:20 -0300

    Agrego token de acceso a los tests de operaciones

--- pladmed/tests/integration_tests/test_operation.py
+++ pladmed/tests/integration_tests/test_operation.py
@@ -4,43 +4,60 @@ import json
 
 class OperationTest(BaseTest):
     def test_creates_traceroute(self):
-        res = self.client.post('/traceroute', json=dict(
-            operation="traceroute",
-            probes=["test_probe", "another_test_probe"],
-            params={
-                "ips": ["192.168.0.0", "192.162.1.1"],
-                "confidence": 0.95
-            }
-        ))
+        access_token = self.register_user()
+
+        res = self.client.post(
+            '/traceroute', 
+            json=dict(
+                operation="traceroute",
+                probes=["test_probe", "another_test_probe"],
+                params={
+                    "ips": ["192.168.0.0", "192.162.1.1"],
+                    "confidence": 0.95
+                }
+            ),
+            headers={'access_token': access_token}
+        )
 
         data = json.loads(res.data)
 
         self.assertEqual(201, res.status_code)
 
     def test_creates_ping(self):
-        res = self.client.post('/ping', json=dict(
-            operation="ping",
-            probes=["test_probe", "another_test_probe"],
-            params={
-                "ips": ["192.168.0.0", "192.162.1.1"],
-                "confidence": 0.95
-            }
-        ))
+        access_token = self.register_user()
+
+        res = self.client.post(
+            '/ping', 
+            json=dict(
+                operation="ping",
+                probes=["test_probe", "another_test_probe"],
+                params={
+                    "ips": ["192.168.0.0", "192.162.1.1"],
+                    "confidence": 0.95
+                }
+            ),
+            headers={'access_token': access_token}
+        )
 
         data = json.loads(res.data)
 
         self.assertEqual(201, res.status_code)
 
     def test_creates_dns(self):
-        res = self.client.post('/dns', json=dict(
-            operation="dns",
-            probes=["test_probe", "another_test_probe"],
-            params={
-                "ips": ["192.168.0.0", "192.162.1.1"]
-            }
-        ))
+        access_token = self.register_user()
+
+        res = self.client.post(
+            '/dns',
+            json=dict(
+                operation="dns",
+                probes=["test_probe", "another_test_probe"],
+                params={
+                    "ips": ["192.168.0.0", "192.162.1.1"]
+                }
+            ),
+            headers={'access_token': access_token}
+        )
 
         data = json.loads(res.data)
 
         self.assertEqual(201, res.status_code)
-
